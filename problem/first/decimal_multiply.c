643b60a431ff759a4f31bc3d757174a84980832b
yasinalibd02 <yasinalibd02@gmail.com>
2022-06-14 19:10:53 +0600

    convert to folder

---
+++ problem/first/decimal_multiply.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    float number1,number2,Multipied;
+
+    printf("enter your two number : ");
+    scanf("%f %f",&number1 ,&number2);
+
+    Multipied=number1*number2;
+       
+   printf("Multipied =%f\n",Multipied);
+
+}
---
+++ problem/if_Else/odd_even.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+    int weather;
+
+    printf("enter your weather number : ");
+    scanf("%d",&weather);
+
+    if (weather %2 ==0)
+    {
+       printf("weather is even ");
+    }
+
+    else
+    {
+        printf("weather is odd");
+    }
+    
+    
+}
