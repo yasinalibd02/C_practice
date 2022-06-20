b0e2ec92760a1f202d5dd56cddcbd20488c6e9ff
yasinalibd02 <yasinalibd02@gmail.com>
2022-06-20 09:28:01 +0600

    solv e problem

---
+++ problem_book/practice.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+
+
+int main(){
+  int taka,total=0;
+  
+  printf("enter your Marks : ");
+
+  scanf("%d",&taka);
+   total +=  taka;
+
+   scanf("%d",&taka);
+    total +=  taka;
+
+
+  scanf("%d",&taka);
+  total +=  taka;
+
+  printf("%d",total);
+  
+  
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+}
