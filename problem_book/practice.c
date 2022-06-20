0c280af42f717ea2676a7bbd70fd99caf0e0619e
yasinalibd02 <yasinalibd02@gmail.com>
2022-06-20 10:02:31 +0600

    practice

--- problem_book/practice.c
+++ problem_book/practice.c
@@ -3,21 +3,26 @@
 
 
 int main(){
-  int taka,total=0;
+  int total,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10;
   
-  printf("enter your Marks : ");
+  printf("enter your taka  : ");
 
-  scanf("%d",&taka);
-   total +=  taka;
+  scanf("%d",&t1);
+  scanf("%d",&t2);
+  scanf("%d",&t3);
+  scanf("%d",&t4);
+  scanf("%d",&t5);
+  scanf("%d",&t6);
+  scanf("%d",&t7);
+  scanf("%d",&t8);
+  scanf("%d",&t9);
+  scanf("%d",&t10);
 
-   scanf("%d",&taka);
-    total +=  taka;
+  total=t1+t2+t3+t4+t5+t6+t7+t8+t9+t10;
 
+ 
 
-  scanf("%d",&taka);
-  total +=  taka;
-
-  printf("%d",total);
+  printf(" Total taka = %d\n",total);
   
   
 
