a398b967e3f6fd83f3a409d5bcb2a2641c72d18f
yasinalibd02 <yasinalibd02@gmail.com>
2022-07-06 23:25:58 +0600

    if first bookproblem solved

---
+++ problem_book/5Elseif/water_position.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main(){
+int Celcius;
+
+printf("enter your Celcius Temparature : ");
+scanf("%d",&Celcius);
+
+if (Celcius < 0)
+{
+  printf("ice");
+
+}
+
+if (Celcius && 0 >= 100)
+{
+  printf("lequade");
+ 
+}
+
+if (Celcius > 100)
+{
+  printf("air");
+}
+
+
+
+
+
+
+
+}
