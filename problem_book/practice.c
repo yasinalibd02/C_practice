99f502d294994071cc1ebcc3f7179d3db5f8b804
yasinalibd02 <yasinalibd02@gmail.com>
2022-07-06 08:14:53 +0600

    if practice

--- problem_book/practice.c
+++ problem_book/practice.c
@@ -1,19 +1,45 @@
 #include<stdio.h>
 
 int main(){
-int number;
+int man1,man2,man3,man4,man5,man6,hundred=0;
 
-printf("enter your number : ");
-scanf("%d",&number);
+printf("enter your amount : ");
+scanf("%d%d%d%d%d%d",&man1,&man2,&man3,&man4,&man5,&man6);
 
-if (number % 3 == 0)
+
+if (man1 >=100)
 {
-  printf("It's multiple of 3\n");
+  hundred=hundred+1;
 }
-if (number %  5 == 0)
+
+if (man2 >=100)
 {
-  printf("It's multiple of 5");
+  hundred=hundred+1;
 }
+
+if (man3 >=100)
+{
+  hundred=hundred+1;
+}
+
+if (man4 >=100)
+{
+  hundred=hundred+2;
+}
+
+if (man5 >=100)
+{
+  hundred=hundred+1;
+}
+
+if (man6 >=100)
+{
+  hundred=hundred+1;
+}
+
+
+printf("hundred = %d",hundred);
+
 
 
 
