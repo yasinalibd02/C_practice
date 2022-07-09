2d630374b7afc84906e32f12cff9ada8142f5150
yasinalibd02 <yasinalibd02@gmail.com>
2022-07-09 12:49:11 +0600

    swap just use number

--- problem_book/practice.c
+++ problem_book/practice.c
@@ -1,46 +1,21 @@
 #include<stdio.h>
 
 int main(){
-int man1,man2,man3,man4,man5,man6,hundred=0;
+  int
+   a=5,
+   b=7;
 
-printf("enter your amount : ");
-scanf("%d%d%d%d%d%d",&man1,&man2,&man3,&man4,&man5,&man6);
+  a = a+b;
 
+  b=a-b;
+  a=a-b;
 
-if (man1 >=100)
-{
-  hundred=hundred+1;
-}
-
-if (man2 >=100)
-{
-  hundred=hundred+1;
-}
-
-if (man3 >=100)
-{
-  hundred=hundred+1;
-}
-
-if (man4 >=100)
-{
-  hundred=hundred+2;
-}
-
-if (man5 >=100)
-{
-  hundred=hundred+1;
-}
-
-if (man6 >=100)
-{
-  hundred=hundred+1;
-}
-
-
-printf("hundred = %d",hundred);
+  printf("%d\n",a);
+  printf("%d",b);
 
 
 
 
+
+  
 }
