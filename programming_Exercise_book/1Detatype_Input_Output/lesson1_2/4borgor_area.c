8b2980d4f8358db812aee2f2c64066248218b441
yasinalibd02 <yasinalibd02@gmail.com>
2022-08-01 22:24:46 +0600

    1_2 complete 5number problem

---
+++ programming_Exercise_book/1Detatype_Input_Output/lesson1_2/4borgor_area.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main(){
+    int armlength,area;
+
+    printf("enter your bahu : ");
+    scanf("%d",&armlength);
+
+  area = armlength*armlength;
+  printf("Area is = %d\n",area);
+
+}
---
+++ programming_Exercise_book/1Detatype_Input_Output/lesson1_2/5ayotokhetro.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main(){
+    int length,width,area;
+
+    printf("enter your lenth : ");
+    scanf("%d",&length);
+
+     printf("enter your width : ");
+    scanf("%d",&width);
+
+  area = length * width;
+  printf("Area is = %d\n",area);
+
+}
